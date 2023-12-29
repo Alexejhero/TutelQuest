4ef429efdf8977d52f6aeaf9a0c96c04eb21fa67
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2023-12-29 20:40:57 +0200

    Add pressure plates

---
+++ Assets/Game/Mechanisms/PressurePlate.cs
@@ -0,0 +1,42 @@
+﻿using SchizoQuest.Game.Players;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SchizoQuest.Game.Mechanisms
+{
+    public class PressurePlate : MonoBehaviour
+    {
+        public bool flipped = false;
+        [HideInInspector] public bool isOn = false;
+        public UnityEvent<bool> onSwitch;
+
+        public GameObject on;
+        public GameObject off;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.GetComponent<Player>())
+            {
+                isOn = !flipped;
+
+                on.SetActive(isOn);
+                off.SetActive(!isOn);
+
+                onSwitch?.Invoke(isOn);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.GetComponent<Player>())
+            {
+                isOn = flipped;
+
+                on.SetActive(isOn);
+                off.SetActive(!isOn);
+
+                onSwitch?.Invoke(isOn);
+            }
+        }
+    }
+}
