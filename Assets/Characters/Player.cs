11efd91492b12cd775c078f5585ce5124a57d75f
w1n7er87 <134517334+w1n7er87@users.noreply.github.com>
2023-12-31 08:21:35 +0100

    some effects
    abomination

--- Assets/Characters/Player.cs
+++ Assets/Characters/Player.cs
@@ -21,6 +21,7 @@ namespace SchizoQuest.Characters
             _renderers = GetComponentsInChildren<SpriteRenderer>();
             respawn.OnReset += (r) =>
             {
+                EffectsManager.Instance.PlayEffect(EffectsManager.Effects.death, 0.3f);
                 if (!inventory.item) return;
                 Carryable item = inventory.item;
                 inventory.Drop(item);
